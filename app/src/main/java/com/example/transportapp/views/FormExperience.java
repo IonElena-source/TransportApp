cdc1cd52c96eda542bc939f55fbf564aed0cfb92
Cristina <ionelenacristina0@gmail.com>
2021-08-04 11:04:10 +0300

    HomeUser

---
+++ app/src/main/java/com/example/transportapp/views/FormExperience.java
@@ -0,0 +1,16 @@
+package com.example.transportapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.transportapp.R;
+
+public class FormExperience extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_experience);
+    }
+}
--- app/src/main/java/com/example/transportapp/views/HomeFragment.java
+++ app/src/main/java/com/example/transportapp/views/HomeFragment.java
@@ -1,20 +1,63 @@
 package com.example.transportapp.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.transportapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeFragment extends Fragment {
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        String uidCurrentUser=firebaseAuth.getCurrentUser().getUid();
+        DocumentReference documentReference=firebaseFirestore.collection("users").document(uidCurrentUser);
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshotUser=task.getResult();
+                    String usernameCurrentUser="Welcome, "+documentSnapshotUser.get("Username")+"";
+                    TextView textViewCurrentUser=getActivity().findViewById(R.id.textViewUser);
+                    textViewCurrentUser.setText(usernameCurrentUser);
+                }
+            }
+        });
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragement_home,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        Button buttonAddExperience=rootView.findViewById(R.id.buttonAddExperience);
+                buttonAddExperience.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent=new Intent(getActivity().getApplicationContext(),FormExperience.class);
+                        startActivity(intent);
+                    }
+                });
+        return rootView;
     }
 }
